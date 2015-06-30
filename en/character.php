d3079963a407887ac24f3a383772250120a73f51
Sergey Leschina <mail@putnik.ws>
2015-06-30 23:30:06 +0300

    Add characters namepsace

---
+++ en/character.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+	'filmography' => 'Filmography',
+
+	'character_of_the_work' => '{1} Character of the work|[2,Inf] Character of the works',
+	'performer' => '{1} Performer|[2,Inf] Performers',
+	'voice_actor' => '{1} Voice actor|[2,Inf] Voice actors',
+
+];

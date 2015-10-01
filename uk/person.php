f585eda2e4680a4190dd47753fea26eae5bec456
Sergey Leschina <mail@putnik.ws>
2015-10-01 03:27:06 +0300

    Add Ukranian translation for person pages

---
+++ uk/person.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+	'born' => 'нар.',
+	'died' => 'пом.',
+
+	'filmography' => 'Фільмографія',
+
+	'actor' => 'Актор',
+	'choreographer' => 'Хореограф',
+	'composer' => 'Композитор',
+	'creator' => 'Творець',
+	'director' => 'Режисер',
+	'executive_producer' => 'Виконавчий продюсер',
+	'operator' => 'Оператор',
+	'producer' => 'Продюсер',
+	'voice_actor' => 'Актор озвучування',
+	'writer' => 'Сценарист',
+
+	'as_character' => 'Як персонаж',
+
+	'language' => '{1} Мова|[2,Inf] Мови',
+	'role' => '{1} Роль|[2,Inf] Ролі',
+
+];

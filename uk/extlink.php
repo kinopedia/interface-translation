e589dcd764c2934e426318c721c121638ccf7ca3
Sergey Leschina <mail@putnik.ws>
2015-10-17 20:04:36 +0300

    Add be, be-tarask an uk translations for extlinks

---
+++ uk/extlink.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	'animator' => 'Аниматор.ru',
+	'commons' => 'Вікісховище',
+	'dfi' => 'Датська національна фільмографія',
+	'imdb' => 'IMDb',
+	'official_website' => 'Офіційний сайт',
+	'website' => 'Сайт',
+	'wikidata' => 'Вікідані',
+	'wikipedia' => 'Вікіпедія',
+
+];
